592711c89114efb4039cf98ab595456befa964d5
Rüstem Akıncık <r.akincikk@gmail.com>
2026-07-01 05:35:04 +0300

    chore: clean up local recording json backups, sql dumps, and queries

--- query.sql
+++
@@ -1,1 +0,0 @@
-SELECT "UserId", SUM("WatchedSeconds") as t FROM "VideoProgresses" GROUP BY "UserId" ORDER BY t DESC LIMIT 10;
--- query2.sql
+++
@@ -1,1 +0,0 @@
-SELECT "Id", "Email", "FirstName", "LastName" FROM "Users" WHERE "Email" LIKE '%outlook.com%' OR "Email" LIKE '%gmail.com%' LIMIT 10;
